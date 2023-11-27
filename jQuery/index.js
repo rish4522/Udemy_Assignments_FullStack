7d092450090376710aeea99ca736afba6d954e13
RishEverec <rishabh.tripathi@mca.christuniversity,in>
2023-11-27 18:00:30 +0530

    Style Manipulations

---
+++ jQuery/index.js
@@ -0,0 +1,5 @@
+$("h1").css("color","red");
+// This is same as writing - document.querySelector("h1").style.color = "blue"; in JavaScript.
+
+$("h1").css("font-size","3rem");
+$("h1").addClass("big-title margin-50");
