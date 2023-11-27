20f2d39cc7d43d814aa70d5e2ae9f670ffd2ae08
RishEverec <rishabh.tripathi@mca.christuniversity,in>
2023-11-27 18:06:32 +0530

    Text Manipulations

--- jQuery/index.js
+++ jQuery/index.js
@@ -3,3 +3,6 @@ $("h1").css("color","red");
 
 $("h1").css("font-size","3rem");
 $("h1").addClass("big-title margin-50");
+
+$("button").html("Manipulating Text");
+// $("body").html("<h2> Adding Heading 2 using jQuery </h2>");
