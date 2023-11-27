5d2e99767637abac5ce62af7fa5d3ba113dc7c03
RishEverec <rishabh.tripathi@mca.christuniversity,in>
2023-11-27 18:15:51 +0530

    Attr Manipulations

--- jQuery/index.js
+++ jQuery/index.js
@@ -6,3 +6,7 @@ $("h1").addClass("big-title margin-50");
 
 $("button").html("Manipulating Text");
 // $("body").html("<h2> Adding Heading 2 using jQuery </h2>");
+
+console.log($("img").attr("src"));
+$("img").attr("src", "https://i.pinimg.com/originals/dd/97/3a/dd973ac116a977c8dd5296b0da504b8c.jpg");
+
